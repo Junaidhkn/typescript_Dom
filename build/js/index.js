67fd1276eddbaea435c17b9772ac58ea2742d7b7
Junaidhkn <junaidhkniazi@gmail.com>
2023-01-19 02:01:48 +0500

    dom

--- build/js/index.js
+++ build/js/index.js
@@ -3,12 +3,19 @@
 // // Non-Null Assertion Operator  '!'  TypeScript Considers it to be not Null
 const btn = document.getElementById('btn');
 const input = document.getElementById('input');
-//  Another Syntax
-input.value;
-btn.addEventListener('click', function () {
-    alert(input.value);
+const form = document.querySelector('form');
+const list = document.getElementById('list');
+// //  Another Syntax
+// (<HTMLInputElement>input).value;
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = input.value;
+    const newLi = document.createElement('li');
+    newLi.append(newItem);
+    list.append(newLi);
     input.value = '';
-});
+};
+form.addEventListener('submit', handleSubmit);
 // // Type Assertions
 const mystery = 'Hello';
 const charLength = mystery.length;
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,22 @@
 // // Non-Null Assertion Operator  '!'  TypeScript Considers it to be not Null
 const btn = document.getElementById('btn')!;
 const input = document.getElementById('input')! as HTMLInputElement;
+const form = document.querySelector('form')!;
+const list = document.getElementById('list')!;
 
-//  Another Syntax
+// //  Another Syntax
 
-(<HTMLInputElement>input).value;
-
-btn.addEventListener('click', function () {
-	alert(input.value);
+// (<HTMLInputElement>input).value;
+const handleSubmit = (e: SubmitEvent) => {
+	e.preventDefault();
+	const newItem = input.value;
+	const newLi = document.createElement('li');
+	newLi.append(newItem);
+	list.append(newLi);
 	input.value = '';
-});
+};
+
+form.addEventListener('submit', handleSubmit);
 
 // // Type Assertions
 
